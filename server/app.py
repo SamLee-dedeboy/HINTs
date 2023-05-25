263440d82d5c847480c1d24147af02624583014c
SamLee-dedeboy <samytleezzz@gmail.com>
2023-05-25 11:05:29 -0700

    add tvcg template

--- server/app.py
+++ server/app.py
@@ -4,14 +4,16 @@ from flask_cors import CORS
 import json
 from datetime import datetime
 from pprint import pprint
+import EventNetwork
 
 app = Flask(__name__)
 CORS(app)
 
+event_network =  EventNetwork(r'../preprocess/data/result/event_hgraph/')
 @app.route("/data/event_network")
 def get_event_network():
     graph = {
-        "nodes": data_manager.event_network.nodes,
-        "links": data_manager.event_network.links,
+        "nodes": event_network.nodes,
+        "links": event_network.links,
     }
     return json.dumps(graph, default=vars)
