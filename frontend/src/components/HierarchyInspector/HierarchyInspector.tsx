88fcaeee054cb0c8e6acee89ce3190863f134dda
SamLee-dedeboy <samytleezzz@gmail.com>
2023-06-19 14:49:52 -0700

    add community verification

--- frontend/src/components/HierarchyInspector/HierarchyInspector.tsx
+++ frontend/src/components/HierarchyInspector/HierarchyInspector.tsx
@@ -1,36 +1,40 @@
 import './HierarchyInspector.css'
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Tree } from 'antd';
 import type { DataNode } from 'antd/es/tree';
 
-function HierarchyInspector(hierarchies) {
+function HierarchyInspector({hierarchies, handleChecked}) {
     const [expandedKeys, setExpandedKeys] = useState<React.Key[]>([]);
     const [checkedKeys, setCheckedKeys] = useState<React.Key[]>([]);
     const [selectedKeys, setSelectedKeys] = useState<React.Key[]>([]);
     const [autoExpandParent, setAutoExpandParent] = useState<boolean>(true);
-    const treeData = useEffect(() => {
-        
-        
+
+    const treeData:DataNode[] = useMemo(() => {
+        return [hierarchies]
     }, [hierarchies])
+
+
     const onExpand = (expandedKeysValue: React.Key[]) => {
-        console.log('onExpand', expandedKeysValue);
+        // console.log('onExpand', expandedKeysValue);
         // if not set autoExpandParent to false, if children expanded, parent can not collapse.
         // or, you can remove all expanded children keys.
         setExpandedKeys(expandedKeysValue);
         setAutoExpandParent(false);
     };
 
-    const onCheck = (checkedKeysValue: React.Key[]) => {
+    function onCheck (checkedKeysValue: React.Key[]) {
         console.log('onCheck', checkedKeysValue);
         setCheckedKeys(checkedKeysValue);
+        handleChecked(checkedKeysValue)
     };
 
     const onSelect = (selectedKeysValue: React.Key[], info: any) => {
-        console.log('onSelect', info);
+        // console.log('onSelect', info);
         setSelectedKeys(selectedKeysValue);
     };
 
     return (
+        treeData &&
         <Tree
         checkable
         onExpand={onExpand}
