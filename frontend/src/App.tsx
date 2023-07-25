bec568657310754e140dce2c634fec2b1bf7c30e
SamLee-dedeboy <samytleezzz@gmail.com>
2023-07-24 17:42:19 -0700

    add contours to overview and refactor layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,21 +6,21 @@ import { server_address } from './shared'
 import type { t_EventHGraph } from './types'
 import './App.css'
 import ClusterOverview from './components/ClusterOverview/ClusterOverview'
+import ClusterDetail from './components/ClusterDetail/ClusterDetail'
 import LevelInput from './components/LevelInput/LevelInput'
 
 function App() {
   const [event_hgraph, setEventHGraph] = useState<t_EventHGraph>()
   const [eventHGraphLoaded, setEventHGraphLoaded] = useState(false)
-  const [communities, setCommunities] = useState<any[]>()
   const [hierarchy, setHierarchy] = useState<any[]>()
   const [contents, setContents] = useState<any[]>()
   const [topic, setTopic] = useState<any>()
   const [level, setLevel] = useState<any>(5)
-  const [enabled_communities, setEnabledCommunities] = useState<any[]>()
-  const [filters, setFilters] = useState({
-      community_size: 5,
-      selected_communities: "all"
-  })
+  const [cluster_data, setClusterData] = useState<any>()
+
+  // flags
+  const [cluster_selected, setClusterSelected] = useState(false)
+  const [cluster_data_fetched, setClusterDataFetched] = useState(false)
 
   useEffect(() => {
       // fetch_communities()
@@ -77,19 +77,19 @@ function App() {
   // }
 
   async function fetchPartition() {
-    console.log("fetching partition", level)
+    console.log("fetching hgraph with partition", level)
     fetch(`${server_address}/data/partition`, {
       method: "POST",
       headers: {
           "Accept": "application/json",
           "Content-Type": "application/json"
       },
-      body: JSON.stringify(level)
+      body: JSON.stringify({ level: level, entity_node_num: 5})
     })
       .then(res => res.json())
-      .then((partition: t_EventHGraph) => {
-        console.log({partition})
-        setEventHGraph(partition)
+      .then((hgraph: t_EventHGraph) => {
+        console.log({hgraph})
+        setEventHGraph(hgraph)
         setEventHGraphLoaded(true)
         // setEnabledCommunities(event_hgraph.communities)
       })
@@ -150,46 +150,72 @@ function App() {
       })
   }
 
+  async function handleClusterSelected(cluster_id) {
+    setClusterSelected(true)
+    setClusterDataFetched(false)
+    console.log("fetching for cluster", cluster_id)
+    fetch(`${server_address}/cluster`, {
+      method: "POST",
+      headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(cluster_id)
+    })
+      .then(res => res.json())
+      .then(cluster_data => {
+        console.log({cluster_data})
+        setClusterData(cluster_data)
+        setClusterDataFetched(true)
+      })
+
+  }
+
 
 
   return (
-    <div className="App">
-      <div className='left-panel'>
-        {/* {
-          enabled_communities && communities && 
-          <Filters communities={communities} enabled_communities={enabled_communities} onChange={fetch_event_hgraph} ></Filters>
-        } */}
+    <div className="App flex w-full h-full">
+      <div className='left-panel flex basis-1/2 h-full'>
+        {
+          !eventHGraphLoaded &&
+          <div className="loading-hint"> Loading... </div>
+        }
+        {
+          eventHGraphLoaded && !cluster_selected && 
+          // <EventHgraph svgId={'event-network'} network_data={event_hgraph} total_communities={event_hgraph?.communities.length || 1}></EventHgraph>
+          <ClusterOverview svgId={"cluster-overview-svg"} graph={event_hgraph!} hierarchies={hierarchy} onNodesSelected={fetchTopic} onClusterSelected={handleClusterSelected} />
+        }
+        {
+          cluster_selected && !cluster_data_fetched && 
+          <div className="loading-hint"> Loading... </div>
+        }
+        {
+          cluster_selected && cluster_data_fetched && 
+          <ClusterDetail svgId={"cluster-detail-svg"} cluster_data={cluster_data} onNodesSelected={fetchTopic}/>
+        }
+      </div>
+      <div className='right-panel flex basis-1/2 w-1/12'>
         {
           hierarchy &&
           <div>
             <button className={"test"} onClick={fetchPartition}> Show Level {level}</button>
             <LevelInput inputValue={level} onChange={setLevel} minValue={0} maxValue={7} />
             <HierarchyInspector hierarchies={hierarchy} handleChecked={handleHierarchyChecked} ></HierarchyInspector>
-            <div className={"topic-viewer"}> {topic} </div>
+            <div className="topic-viewer w-full"> {topic} </div>
           </div>
         }
-
-      </div>
-      <div className='right-panel'>
-        {/* {
-        } */}
-        {
-          eventHGraphLoaded && 
-          // <EventHgraph svgId={'event-network'} network_data={event_hgraph} total_communities={event_hgraph?.communities.length || 1}></EventHgraph>
-          <ClusterOverview svgId={"cluster-svg"} graph={event_hgraph} hierarchies={hierarchy} onNodesSelected={fetchTopic} />
-        }
-        <div className={'description-panel'}>
+        <div className='flex basis-1/2'>
           {
             contents &&
             Object.keys(contents).map(comm_label => {
               return (
-                <div className='cluster-content-container' key={comm_label}>
+                <div className='cluster-content-container w-full h-1/2' key={comm_label}>
                   <p> comm size: {contents[comm_label].length}</p>
-                  <div className='cluster-node-list-container'>
+                  <div className='cluster-node-list-container flex flex-col overflow-scroll border border-solid border-black h-9/10'>
                     {
                     contents[comm_label].map(node => {
                       return (
-                        <div className='cluster-node-container' key={node.trigger}>
+                        <div className='cluster-node-container border-b border-solid border-black' key={node.trigger}>
                           <p> Event: {node.trigger}</p>
                           <p> arguments: {node.arguments}</p>
                           <p> summary: {node.summary}</p>
@@ -203,6 +229,7 @@ function App() {
             })
           }
         </div>
+
       </div>
     </div>
   )
